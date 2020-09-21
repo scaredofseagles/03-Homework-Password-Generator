cfcc133b39f26c1b0fd80e88f86e767dc3b474c2
Dailey Kaze <daileykaze@gmail.com>
2020-09-20 22:42:49 -0400

    Added functions to generate password

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-// Assignment Code
+
+// Assign code to button
 var generateBtn = document.querySelector("#generate");
 
+generateBtn.addEventListener("click", userPrompt);
+
+
 // User Prompts and Confirms (length, uppercase, lowercase, numbers, and symbols)
-var finalPassword = ""
 
-var isLength = prompt("How long do want your password to be?")
-var hasUpper = confirm("Do you want to use uppercase letters?")
-var hasLower = confirm("Do you want to use lowercase letters?")
-var hasNumber = confirm("Do you want to use numbers?")
-var hasSymbol = confirm("Do you want to use symbols?")
+function userPrompt() {
+  console.log('reached userPrompt function')
+  
+  isLength = prompt("How long do want your password to be?")
+  var hasUpper = confirm("Do you want to use uppercase letters?")
+  var hasLower = confirm("Do you want to use lowercase letters?")
+  var hasNumber = confirm("Do you want to use numbers?")
+  var hasSymbol = confirm("Do you want to use symbols?")
+  
+  /* if ( hasUpper == false ) && ( hasLower == false ) && ( hasNumber == false ) && ( hasSymbol == false ) {
+    alert("Please choose one of the choices.")
+  } 
+  else {
+      generatePassword
+  } */
 
-
-// Append characters to a final character set
-
-var finalCharSet = ""
-if ( hasUpper ) finalCharSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-if (hasLower) finalCharSet += "abcdefghijklmnopqrstuvwxyz"
-if (hasNumber) finalCharSet += "1234567890"
-if (hasSymbol) finalCharSet += "!@#$%?&*()"
-
-// Loop through final character set for random indexes and add to password text
-
-for (i=0; i > isLength; i++){
-  var characterIdx = parseInt(Math.random * finalCharSet.length)
-  finalPassword += finalCharSet[characterIdx]
-
+  // Append characters to a final character set
+  var finalCharSet = ""
+  if ( hasUpper ) finalCharSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (hasLower) finalCharSet += "abcdefghijklmnopqrstuvwxyz";
+  if (hasNumber) finalCharSet += "1234567890";
+  if (hasSymbol) finalCharSet += "!@#$%?&*()";
+  
+  
+  generatePassword(finalCharSet, isLength)
+      
 }
-console.log(`Your generated password is: ${finalPassword} `)
 
+// Generate set of random alphanumeric characters
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function generatePassword(finalCharSet, isLength) {
+  var finalPassword = ""
+  console.log("reached the generatePassword function")
+  
+
+  for (i=1; i < isLength; i++ ) {
+    var characterIdx = Math.floor(Math.random()* finalCharSet.length + 1)
+    finalPassword += finalCharSet[characterIdx]
+  }
+
+  console.log("Your generated password is: " + finalPassword)
+  
+  // Replace text in html with generated password  
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = finalPassword;
+
+
 
 }
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
